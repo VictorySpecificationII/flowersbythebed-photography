ef1ce81c6c96477f43aca0845a5d0ddf31114e90
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-08 20:12:11 +0300

    added image to home section

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import HomeImg from "../images/Home/Home.jpeg";
+import "../App.css";
 
 const Home = () => {
   return (
-    <section id="home" className="section">
+    <section
+      id="home"
+      className="section"
+      style={{
+        backgroundImage: `url(${HomeImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        color: "white",       // text color over image
+      }}
+    >
       <h1>Home</h1>
       <p>Welcome to my portfolio!</p>
     </section>
