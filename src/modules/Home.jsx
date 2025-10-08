ae47035f90dece7b03ab5561af514e467e346f87
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-08 20:52:16 +0300

    basic carousel working

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,22 +1,60 @@
 import React from "react";
-import HomeImg from "../images/Home/Home.jpeg";
-import "../App.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import img1 from "../images/Home/Home.jpeg";
+import img2 from "../images/Home/Home2.jpeg";
+import img3 from "../images/Home/Home3.jpeg";
+import img4 from "../images/Home/Home4.jpeg";
 
 const Home = () => {
+  const images = [img1, img2, img3, img4];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+  };
+
   return (
-    <section
-      id="home"
-      className="section"
-      style={{
-        backgroundImage: `url(${HomeImg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        color: "white",       // text color over image
-      }}
-    >
-      <h1>Home</h1>
-      <p>Welcome to my portfolio!</p>
+    <section id="home" style={{ height: "100vh", position: "relative" }}>
+      <Slider {...settings}>
+        {images.map((img, idx) => (
+          <div key={idx} style={{ height: "100vh" }}>
+            <img
+              src={img}
+              alt={`Slide ${idx + 1}`}
+              style={{
+                width: "100%",
+                height: "100vh",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        ))}
+      </Slider>
+
+      {/* Overlay text */}
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          color: "white",
+          textAlign: "center",
+          zIndex: 2,
+        }}
+      >
+        <h1>Home</h1>
+        <p>Welcome to my portfolio!</p>
+      </div>
     </section>
   );
 };
