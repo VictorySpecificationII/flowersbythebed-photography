0e061f3439bd1152f5a615eee8ae084a9cd716ac
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-08 20:19:37 +0300

    added image to portfolio section, placeholder

--- src/modules/Portfolio.jsx
+++ src/modules/Portfolio.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import PortfolioImg from "../images/Portfolio/Portfolio.jpeg";
+import "../App.css";
 
 const Portfolio = () => {
   return (
-    <section id="portfolio" className="section">
+    <section
+      id="portfolio"
+      className="section"
+      style={{
+        backgroundImage: `url(${PortfolioImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        color: "white",       // text color over image
+      }}
+    >
       <h1>Portfolio</h1>
-      <p>Some of my best work.</p>
+      <p>This is my Portfolio!</p>
     </section>
   );
 };
