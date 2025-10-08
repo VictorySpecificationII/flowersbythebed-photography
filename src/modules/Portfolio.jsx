da2216f70b95e57a7aa5c9466d74ae6fa0fd6bff
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-09 01:07:35 +0300

    added another 4 placeholder pics to test section expansion

--- src/modules/Portfolio.jsx
+++ src/modules/Portfolio.jsx
@@ -9,10 +9,14 @@ import img5 from "../images/Portfolio/Portfolio5.jpeg";
 import img6 from "../images/Portfolio/Portfolio6.jpeg";
 import img7 from "../images/Portfolio/Portfolio7.jpeg";
 import img8 from "../images/Portfolio/Portfolio8.jpeg";
+import img9 from "../images/Portfolio/Portfolio9.jpeg";
+import img10 from "../images/Portfolio/Portfolio10.jpeg";
+import img11 from "../images/Portfolio/Portfolio11.jpeg";
+import img12 from "../images/Portfolio/Portfolio12.jpeg";
 
 
 const Portfolio = () => {
-  const images = [img1, img2, img3, img4, img5, img6, img7, img8];
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12];
 
   return (
     <section id="portfolio" style={{ minHeight: "100vh", background: "#fff" }}>
