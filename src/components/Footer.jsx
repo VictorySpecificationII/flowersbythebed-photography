ae934f0280b508d6407e907be14ade2baeb9f7ae
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-09 21:16:12 +0300

    switched from unicode to react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { FaInstagram, FaGithub } from "react-icons/fa";
+import { FaGithub, FaHeart } from "react-icons/fa";
 import "../styles/Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div style={{ textAlign: "center", color: "#aaa", fontSize: "0.9rem" }}>
-        Made with ❤️ in ReactJS by
+      <div style={{ textAlign: "center", color: "#aaa", fontSize: "0.9rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "0.2rem" }}>
+        Made with 
+        <FaHeart size={12} color="red" />
+        in ReactJS by 
         <a
           href="https://www.github.com/VictorySpecificationII"
           target="_blank"
           rel="noreferrer"
           style={{ marginLeft: "0.3rem", color: "#fff" }}
         >
-          <FaGithub size={20} />
+          <FaGithub size={14} />
         </a>
       </div>
     </footer>
@@ -21,4 +23,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
