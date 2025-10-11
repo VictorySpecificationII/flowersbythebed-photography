9c5051b3c580f94931373c31c53dcfa82b529828
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-11 21:48:20 +0300

    added dates to project cards

--- src/modules/Projects.jsx
+++ src/modules/Projects.jsx
@@ -3,33 +3,39 @@ import project1 from "../images/Home/Home.jpeg";
 import project2 from "../images/Home/Home2.jpeg";
 import project3 from "../images/Home/Home3.jpeg";
 import project4 from "../images/Home/Home4.jpeg";
-import project5 from "../images/Home/Home.jpeg";
-import project6 from "../images/Home/Home2.jpeg";
-import project7 from "../images/Home/Home3.jpeg";
-import project8 from "../images/Home/Home4.jpeg";
+
+const projects = [
+  { img: project1, title: "Project Alpha", date: "Jan 2024" },
+  { img: project2, title: "Project Beta", date: "Feb 2024" },
+  { img: project3, title: "Project Gamma", date: "Mar 2024" },
+  { img: project4, title: "Project Delta", date: "Apr 2024" },
+];
 
 export default function ProjectsSection() {
   return (
     <div className="projects-container">
-
-      {/* Section Title */}
       <h2 className="projects-title">My Projects</h2>
-
-      {/* Grid of Projects */}
       <div className="projects-grid">
-        {[project1, project2, project3, project4, project5, project6, project7, project8].map((img, idx) => (
-          <div className="project-card" key={idx}>
-            <img src={img} alt={`Project ${idx + 1}`} />
+        {projects.map((proj, idx) => (
+          <div
+            className="project-card"
+            key={idx}
+            onClick={() => alert(`Open gallery for ${proj.title}`)}
+          >
+            <img src={proj.img} alt={proj.title} />
+            <div className="project-overlay">
+              <h3>{proj.title}</h3>
+              <span>{proj.date}</span>
+            </div>
           </div>
         ))}
       </div>
 
-      {/* Optional custom content section */}
       <section className="custom-section">
         <h2>Special Feature</h2>
         <p>This area is flexible. Add text, media, or promo content here.</p>
       </section>
-
     </div>
   );
 }
+
