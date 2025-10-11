09911183388c5f00c2da94a70bc6d10939193faa
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-11 19:58:23 +0300

    made a start on the about me page

--- src/modules/AboutMe.jsx
+++ src/modules/AboutMe.jsx
@@ -3,7 +3,12 @@ import "./submodules/AboutMe/AboutMe.css";
 export default function AboutMe() {
   return (
     <div className="aboutme-container">
-      <h1>Hello World</h1>
+      <div className="parallax-bg"></div>
+      <div className="aboutme-content">
+        <h1>Hello World</h1>
+        <p>This is my About Me section with a parallax background!</p>
+      </div>
     </div>
   );
 }
+
