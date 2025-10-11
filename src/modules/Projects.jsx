b9acbcd16f5ff8bbec91e234f9797750619c11d1
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-11 21:21:26 +0300

    somewhat better projects page

--- src/modules/Projects.jsx
+++ src/modules/Projects.jsx
@@ -4,20 +4,28 @@ import project2 from "../images/Home/Home2.jpeg";
 import project3 from "../images/Home/Home3.jpeg";
 import project4 from "../images/Home/Home4.jpeg";
 
-export default function Projects() {
-  const projects = [project1, project2, project3, project4];
-
+export default function ProjectsSection() {
   return (
     <div className="projects-container">
+
+      {/* Section Title */}
       <h2 className="projects-title">My Projects</h2>
+
+      {/* Grid of Projects */}
       <div className="projects-grid">
-        {projects.map((img, idx) => (
-          <div key={idx} className="project-card">
+        {[project1, project2, project3, project4].map((img, idx) => (
+          <div className="project-card" key={idx}>
             <img src={img} alt={`Project ${idx + 1}`} />
           </div>
         ))}
       </div>
+
+      {/* Optional custom content section */}
+      <section className="custom-section">
+        <h2>Special Feature</h2>
+        <p>This area is flexible. Add text, media, or promo content here.</p>
+      </section>
+
     </div>
   );
 }
-
